7120e5887e26593c98c1b97d57b479e1e2d6c5d0
Bk0771 <117773467+Bk0771@users.noreply.github.com>
2023-06-27 14:29:13 -0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,16 +1,6 @@
-# Ussing puppet - intall Web Server Nginx and confugure redirect_me
-# Install Nginx
-exec {'Install_nginx':
+# Installs a Nginx server
+
+exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ;\
-               sudo apt-get -y install nginx ;\
-               sudo chown -R ubuntu /var/www ;\
-               sudo service nginx start',
-}
-# Configure redirect_me
-exec {'Configure_redirect':
-  provider => shell,
-  command  => 'echo "Hello World" > /var/www/html/index.nginx-debian.html ;\
-               sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ;\
-               sudo service nginx restart',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
